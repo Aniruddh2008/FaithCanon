7bebbf8aab103c9ebd81040706b7ed951b1216ef
Kusumakar <avasthi3july@gmail.com>
2017-12-05 09:25:23 +0530

    package name

--- app/src/main/java/faithcanon/activity/SplashScreen.java
+++ app/src/main/java/faithcanon/activity/SplashScreen.java
@@ -1,19 +1,32 @@
 package faithcanon.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.faithcanon.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 /**
  * Created by A12PCHBR on 11/21/2017.
  */
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends AppCompatActivity {
 
+    private static final String TAG = SplashScreen.class.getSimpleName();
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
 
@@ -21,7 +34,9 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        printHashKey(SplashScreen.this);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         new Handler().postDelayed(new Runnable() {
 
             /*
@@ -41,5 +56,19 @@ public class SplashScreen extends Activity {
             }
         }, SPLASH_TIME_OUT);
     }
-
+    public static void printHashKey(Context pContext) {
+        try {
+            PackageInfo info = pContext.getPackageManager().getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i(TAG, "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e(TAG, "printHashKey()", e);
+        }
+    }
 }
